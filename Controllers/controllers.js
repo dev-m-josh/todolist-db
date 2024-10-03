9f56ca74e784a23d735a522d387fa8b6446a1ec4
Joshua Mutambuki <mutambukijoshua2@gmail.com>
2024-10-03 17:09:36 +0300

    getting todos for a specific user

--- Controllers/controllers.js
+++ Controllers/controllers.js
@@ -77,5 +77,22 @@ function editTodo(req, res) {
 };
 
 
+//GET ALL TODOS OF A SPECIFIC USER
+function specificUserTodos(req, res) {
+    let requestedUser = req.params.userId;
+    let { page, pageSize} = req.query;
+    let offset = (Number(page)-1) * Number(pageSize);
+    new sql.Request().query(`SELECT todo_id, todo_title, todo_description, todo_deadline, todo_status 
+FROM todos
+WHERE user_id = ${requestedUser} ORDER BY todo_id OFFSET ${offset} ROWS FETCH NEXT ${pageSize} ROWS ONLY`, (err, result)=>{
+    if (err) {
+        console.log("Error occured in query", err);
+    } else {
+        res.json(result.recordset);
+    };
+});
+};
 
-module.exports = { getAllTodos, getSingleTodo, addNewTodo, deleteSingleTodo, editTodo };
+
+
+module.exports = { getAllTodos, getSingleTodo, addNewTodo, deleteSingleTodo, editTodo, specificUserTodos };
--- Routers/routers.js
+++ Routers/routers.js
@@ -1,12 +1,13 @@
 
 const router = require("express").Router();
-const { getAllTodos, getSingleTodo, addNewTodo, deleteSingleTodo, editTodo } = require("../Controllers/controllers");
+const { getAllTodos, getSingleTodo, addNewTodo, deleteSingleTodo, editTodo, specificUserTodos } = require("../Controllers/controllers");
 
 //GET ALL TODOS
 router.get('/todos', getAllTodos);
 router.get('/todos/:todoId', getSingleTodo);
 router.post('/todos', addNewTodo);
 router.delete('/todos/:todoId', deleteSingleTodo);
-router.post('/todos/:todoId', editTodo)
+router.post('/todos/:todoId', editTodo);
+router.get('/todos/user/:userId', specificUserTodos);
 
 module.exports = { router };
