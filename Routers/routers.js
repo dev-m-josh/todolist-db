fc34cc51b24e3c1788a18b646ab1764cb42c7741
Joshua Mutambuki <mutambukijoshua2@gmail.com>
2024-10-04 14:02:58 +0300

    users routers

--- Routers/routers.js
+++ Routers/routers.js
@@ -1,13 +1,20 @@
 
 const router = require("express").Router();
-const { getAllTodos, getSingleTodo, addNewTodo, deleteSingleTodo, editTodo, specificUserTodos } = require("../Controllers/controllers");
+const { getAllTodos, getSingleTodo, addNewTodo, deleteSingleTodo, editTodo, specificUserTodos, addNewUser, deleteSingleUser, editUser, getAllUsers,getSingleUser } = require("../Controllers/controllers");
 
-//GET ALL TODOS
+//TODOS ROUTES
 router.get('/todos', getAllTodos);
 router.get('/todos/:todoId', getSingleTodo);
 router.post('/todos', addNewTodo);
 router.delete('/todos/:todoId', deleteSingleTodo);
 router.post('/todos/:todoId', editTodo);
+
+//USERS ROUTES
+router.get('/users', getAllUsers);
+router.get('/users/:userId', getSingleUser);
+router.post('/users', addNewUser);
+router.delete('/users/:userId', deleteSingleUser);
+router.post('/users/:userId', editUser);
 router.get('/todos/user/:userId', specificUserTodos);
 
 module.exports = { router };
