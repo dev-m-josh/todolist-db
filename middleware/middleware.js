16cdf115d836d24f5ee14bc03a035ab069dac1b8
Joshua Mutambuki <mutambukijoshua2@gmail.com>
2024-10-08 13:44:50 +0300

    edits

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,22 +1,26 @@
 const jwt = require('jsonwebtoken')
 
 function verifyToken(req, res, next) {
-    
+try {
     const bearerHeader = req.headers['authorization'];
     if (bearerHeader) {
     //SPLIT bearerHeader AND GET THE TOKEN ONLY
     const token = bearerHeader.split(' ')[1];
-        jwt.verify(token, 'secretkey', (err, token)=>{
+        jwt.verify(token, 'secretkey', (err, authData)=>{
             if (err) {
                 return res.sendStatus(403)
             };
-            req.token = token
+            req.token = authData
             next();
         });
-
+    
     }else{
         res.sendStatus(401);
     };
+    } catch (error) {
+        res.json(error)
+    }
+
 };
 
 
